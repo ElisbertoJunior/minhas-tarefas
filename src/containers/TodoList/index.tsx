40262d17d4495b848fe549e7b8aa8a0ec33e8bb6
Elisberto Junior <elisberto_jr@hotmail.com>
2023-04-22 18:25:15 -0300

    Adicionando card de tarefa

--- src/containers/TodoList/index.tsx
+++ src/containers/TodoList/index.tsx
@@ -1,8 +1,57 @@
+import { describe } from 'node:test'
+import Task from '../../components/Task'
+import * as S from './styles'
+
+const tasks = [
+  {
+    title: 'Estudar TypeScript',
+    description: 'Assistir modulo 31',
+    priority: 'importante',
+    status: 'pendente'
+  },
+  {
+    title: 'Estudar TypeScript',
+    description: 'Assistir modulo 31',
+    priority: 'importante',
+    status: 'pendente'
+  },
+  {
+    title: 'Estudar TypeScript',
+    description: 'Assistir modulo 31',
+    priority: 'importante',
+    status: 'pendente'
+  },
+  {
+    title: 'Estudar TypeScript',
+    description: 'Assistir modulo 31',
+    priority: 'importante',
+    status: 'pendente'
+  },
+  {
+    title: 'Estudar TypeScript',
+    description: 'Assistir modulo 31',
+    priority: 'importante',
+    status: 'pendente'
+  }
+]
+
 const TodoList = () => {
   return (
-    <main>
+    <S.Container>
       <p>2 tarefas marcadas como: &quot;categoriae&quot; &quot;termo&quot;</p>
-    </main>
+      <ul>
+        {tasks.map(({ description, priority, status, title }) => (
+          <li key={title}>
+            <Task
+              title={title}
+              description={description}
+              priority={priority}
+              status={status}
+            />
+          </li>
+        ))}
+      </ul>
+    </S.Container>
   )
 }
 
